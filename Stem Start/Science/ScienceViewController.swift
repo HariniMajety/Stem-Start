59e63ef6091df1b4e00203bcdc60d5ad15147d03
Harini Majety <harinipmajety@hotmail.com>
2021-07-01 10:56:55 -0500

    Added Environmental Scientist Connection

--- Stem Start/Science/ScienceViewController.swift
+++ Stem Start/Science/ScienceViewController.swift
@@ -15,15 +15,14 @@ class ScienceViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func enviromentalScientist() {
+        let vc = storyboard?.instantiateViewController(identifier: "EnvironmentalScientist") as! EnvironmentalScientistViewController
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
-    */
+    
+    
+    
 
+ 
 }
