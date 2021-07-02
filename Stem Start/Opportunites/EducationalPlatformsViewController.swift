550d55e8d849ca153e43a578dc5b5664abe2be6c
Momitha <kmomitha@gmail.com>
2021-07-01 17:59:48 -0700

    Added links.

--- Stem Start/Opportunites/EducationalPlatformsViewController.swift
+++ Stem Start/Opportunites/EducationalPlatformsViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class EducationalPlatformsViewController: UIViewController {
-
+    
     @IBOutlet weak var edutopiaTextView: UITextView!
     
     @IBOutlet weak var codecademyTextView: UITextView!
@@ -23,98 +23,155 @@ class EducationalPlatformsViewController: UIViewController {
     
     @IBOutlet weak var ngcpTextView: UITextView!
     
+    
+    
+    
+    
+   @IBAction func edutopiaButtonClicked(_ sender: Any) {
+        if let url = NSURL(string: "https://www.edutopia.org/article/STEM-resources-downloads") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func codecademyButtonClicked(_ sender: Any) {
+        if let url = NSURL(string: "https://www.codecademy.com/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func girlswhocodeButtonClicked(_ sender: Any) {
+        if let url = NSURL(string:"https://girlswhocode.com/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func codeorgButtonClicked(_ sender: Any) {
+        if let url = NSURL(string: "https://code.org/learn") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func khanAcademyButtonClicked(_ sender: Any) {
+        if let url = NSURL(string: "https://www.khanacademy.org/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    
+    @IBAction func SociWomenEngiButtonClicked(_ sender: Any) {
+        if let url = NSURL(string: "https://swe.org/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    
+    @IBAction func NatiGirCollabProj(_ sender: Any) {
+        if let url = NSURL(string:"https://ngcproject.org/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateEdutopiaTextView()
-        updateCodecademyTextView()
-        updateGWCTextView()
-        updateCodeOrgTextView()
-        updateKATextView()
-        updateSWETextView()
-        updateNCGPTextView()
+//        updateEdutopiaTextView()
+//        updateCodecademyTextView()
+//        updateGWCTextView()
+//        updateCodeOrgTextView()
+//        updateKATextView()
+//        updateSWETextView()
+//        updateNCGPTextView()
 
     }
     
-    func updateEdutopiaTextView(){
-        let path = "https://www.edutopia.org/article/STEM-resources-downloads"
-        let text = (edutopiaTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Edutopia")
-        let font = edutopiaTextView.text
-        let color = edutopiaTextView.textColor
-        edutopiaTextView.attributedText = attributedString
-        edutopiaTextView.text = font
-        edutopiaTextView.textColor = color
-    }
+//    func updateEdutopiaTextView(){
+//        let path = "https://www.edutopia.org/article/STEM-resources-downloads"
+//        let text = (edutopiaTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Edutopia")
+//        let font = edutopiaTextView.text
+//        let color = edutopiaTextView.textColor
+//        edutopiaTextView.attributedText = attributedString
+//        edutopiaTextView.text = font
+//        edutopiaTextView.textColor = color
+//    }
     
    
     
-    func updateCodecademyTextView(){
-        let path = "https://www.codecademy.com/"
-        let text = (codecademyTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Codecademy")
-        let font = codecademyTextView.text
-        let color = codecademyTextView.textColor
-        codecademyTextView.attributedText = attributedString
-        codecademyTextView.text = font
-        codecademyTextView.textColor = color
+//    func updateCodecademyTextView(){
+//        let path = "https://www.codecademy.com/"
+//        let text = (codecademyTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Codecademy")
+//        let font = codecademyTextView.text
+//        let color = codecademyTextView.textColor
+//        codecademyTextView.attributedText = attributedString
+//        codecademyTextView.text = font
+//        codecademyTextView.textColor = color
+//    }
+    
+//    func updateGWCTextView(){
+//        let path = "https://girlswhocode.com/"
+//        let text = (gwcTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Girls Who Code")
+//        let font = gwcTextView.text
+//        let color = gwcTextView.textColor
+//        gwcTextView.attributedText = attributedString
+//        gwcTextView.text = font
+//        gwcTextView.textColor = color
+//    }
+//
+//    func updateCodeOrgTextView(){
+//        let path = "https://code.org/learn"
+//        let text = (codeOrgTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Code.org")
+//        let font = codeOrgTextView.text
+//        let color = codeOrgTextView.textColor
+//        codeOrgTextView.attributedText = attributedString
+//        codeOrgTextView.text = font
+//        codeOrgTextView.textColor = color
     }
     
-    func updateGWCTextView(){
-        let path = "https://girlswhocode.com/"
-        let text = (gwcTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Girls Who Code")
-        let font = gwcTextView.text
-        let color = gwcTextView.textColor
-        gwcTextView.attributedText = attributedString
-        gwcTextView.text = font
-        gwcTextView.textColor = color
-    }
+//    func updateKATextView(){
+//        let path = "https://www.khanacademy.org/"
+//        let text = (kaTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Khan Academy")
+//        let font = kaTextView.text
+//        let color = kaTextView.textColor
+//        kaTextView.attributedText = attributedString
+//        kaTextView.text = font
+//        kaTextView.textColor = color
+    //}
     
-    func updateCodeOrgTextView(){
-        let path = "https://code.org/learn"
-        let text = (codeOrgTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Code.org")
-        let font = codeOrgTextView.text
-        let color = codeOrgTextView.textColor
-        codeOrgTextView.attributedText = attributedString
-        codeOrgTextView.text = font
-        codeOrgTextView.textColor = color
-    }
+//    func updateSWETextView(){
+//        let path = "https://swe.org/"
+//        let text = (sweTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Society of Women Engineers")
+//        let font = sweTextView.text
+//        let color = sweTextView.textColor
+//        sweTextView.attributedText = attributedString
+//        sweTextView.text = font
+//        sweTextView.textColor = color
+//    }
     
-    func updateKATextView(){
-        let path = "https://www.khanacademy.org/"
-        let text = (kaTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Khan Academy")
-        let font = kaTextView.text
-        let color = kaTextView.textColor
-        kaTextView.attributedText = attributedString
-        kaTextView.text = font
-        kaTextView.textColor = color
-    }
-    
-    func updateSWETextView(){
-        let path = "https://swe.org/"
-        let text = (sweTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: "Society of Women Engineers")
-        let font = sweTextView.text
-        let color = sweTextView.textColor
-        sweTextView.attributedText = attributedString
-        sweTextView.text = font
-        sweTextView.textColor = color
-    }
-    
-    func updateNCGPTextView(){
-        let path = "https://ngcproject.org/"
-        let text = (ngcpTextView.text)!
-        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: " National Girls Collaborative Project")
-        let font = ngcpTextView.text
-        let color = ngcpTextView.textColor
-        ngcpTextView.attributedText = attributedString
-        ngcpTextView.text = font
-        ngcpTextView.textColor = color
-    }
-    
-  
+//    func updateNCGPTextView(){
+//        let path = "https://ngcproject.org/"
+//        let text = (ngcpTextView.text)!
+//        let attributedString = NSAttributedString.makeHyperLink(for: path, in: text, as: " National Girls Collaborative Project")
+//        let font = ngcpTextView.text
+//        let color = ngcpTextView.textColor
+//        ngcpTextView.attributedText = attributedString
+//        ngcpTextView.text = font
+//        ngcpTextView.textColor = color
+//    }
+//
+//
    
     
     
@@ -125,4 +182,4 @@ class EducationalPlatformsViewController: UIViewController {
 
  
 
-}
+//}
